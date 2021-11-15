030074e0030e1701bc5dafb97cafacc9c37a4b5a
Seeky <seek916@gmail.com>
2021-11-14 18:27:05 -0800

    fun stuff

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
-var search = function() {
+function search() {
+    console.log("searching");
     let url = "https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude=current,minutely,hourly&appid=9f61e2c64b7d7564d396645992b24749"
     getWeatherText(url);
+    console.log(getWeatherText);
 }
 
 
-var gotPosition = function(pos) {
+function gotPosition(pos) {
     var lat = pos.coords.latitude;
     var long = pos.coords.longitude;
     getForecast(lat,long);
 }
 
-var getForecast = function(lat, long) {
+function getForecast(lat, long) {
     let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=current,minutely,hourly&appid=9f61e2c64b7d7564d396645992b24749"
     getWeatherText(url);
 }
@@ -21,7 +23,7 @@ async function getWeatherText(url) {
     parseWeather(weatherText);
 }
 
-var parseWeather = function(weatherText) {
+function parseWeather(weatherText) {
     var weatherJSON = JSON.parse(weatherText);
     var dailyForecast = weatherJSON.daily;
     for (x = 0; x < dailyForecast.length; x++) {
@@ -43,18 +45,19 @@ var parseWeather = function(weatherText) {
     }
 }
 
-var displayWeatherDay = function(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity) {
+function displayWeatherDay(dayOfWeek, description, icon, sunrise, sunset, highTemp, lowTemp, humidity) {
     var out = "<div class='dayBox'><img src='http://openweathermap.org/img/wn/" + icon + ".png'/>";
     out += "<h2>" + dayOfWeek + "</h2>";
     out += "<h3>" + description + "</h3>";
     out += "<p>Sunrise:" + sunrise + "</p>";
     out += "<p>Sunset:" + sunset + "</p>";
     out += "<p>Humidity: " + humidity + "</p>";
-    out += "<p>Windspeed: " + Math.round(wind_speed) + "</p></div>";
+    console.log(windSpeed);
+    out += "<p>Windspeed: " + Math.round(windSpeed) + "</p></div>";
     document.getElementById("forecast").innerHTML += out;
 }
 
-var getDayOfWeek = function(dayNum) {
+function getDayOfWeek(dayNum) {
     var weekday = new Array(7);
     weekday[0] = "Sunday";
     weekday[1] = "Monday";
@@ -67,13 +70,13 @@ var getDayOfWeek = function(dayNum) {
     return (weekday[dayNum]);
 }
 
-var kToF = function(kelvinTemp) {
+function kToF(kelvinTemp) {
     const celsius = kelvinTemp - 273;
-    const fahrenheit = Match.floor(celsius * (9 / 5) + 32);
+    const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
     return fahrenheit
 }
 
-var timestampToTime = function(timeStamp) {
+function timestampToTime(timeStamp) {
     var date = new Date(timeStamp * 1000);
     var hours = date.getHours();
     var minutes = "";
@@ -85,4 +88,4 @@ var timestampToTime = function(timeStamp) {
     return hours + ":" + minutes;
     }
     
-navigator.geolocation.getCurrentPosition(gotPosition);
+//navigator.geolocation.getCurrentPosition(gotPosition);
